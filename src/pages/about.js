d2d7a610280bbe5db6d48ea5ce0ad5b583faa1e8
jlindner22 <jenlindner22@gmail.com>
2020-06-22 13:34:44 -0400

    added new picture to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,22 +2,23 @@ import React from 'react'
 import styles from './about.module.css'
 import Container from '../components/container'
 import Layout from "../components/layout"
-import PG from "../../content/assets/PG2015.jpeg"
-import TootooTable from "../../content/assets/TootooTable.jpeg"
+// import PG from "../../content/assets/PG2015.jpeg"
+// import TootooTable from "../../content/assets/TootooTable.jpeg"
+import deck from "../../content/assets/deck.jpeg"
 import SEO from "../components/seo"
 
 const User = props => (
     <div className={styles.user}>
-        <p className={styles.excerpt}>{props.excerpt}</p>
+        <div>
+        <img src={props.avatar} className={styles.avatar} alt="Jen and Tootoo" />
+        </div>
         <br></br>
-        <div className={styles.flex}>
-        <div>
-        <img src={props.avatar} className={styles.avatar} alt="Jen" />
-        </div>
-        <div>
+        <p className={styles.excerpt}>{props.excerpt}</p>
+        {/* <div className={styles.flex}> */}
+        {/* <div>
         <img className={styles.tootoo} src={props.image} alt="Tootoo"/>
-        </div>
-        </div>
+        </div> */}
+        {/* </div> */}
     </div>
 )
 
@@ -35,8 +36,7 @@ export default function About() {
                 After graduating from Colgate University with a BA in Psychology and working in Human Resources, I realized that I wanted to focus on a more technology-based career. I completed Flatiron School's Immersive Software Engineering program in New York City, where I learned how to build projects in Ruby on Rails, JavaScript, React, and Redux.
                 I'm currently studying up on algorithms and Python, and I'm looking forward to building my first project in Python!
                 I also have a maltipoo named Tootoo who loves to code with me (or step on my keyboard, either works) - try to tell us apart!" 
-                avatar={PG}
-                image={TootooTable}
+                avatar={deck}
                 />
                 <h2 style={{ color: `navy`}}>My Technical Skills</h2>
                 <div style={{ color: `navy`, textAlign: `left` }}>
