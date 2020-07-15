b15abf1ab754f2fc7a368e23afcd3270c33e539b
jlindner22 <jenlindner22@gmail.com>
2020-07-15 14:02:29 -0400

    fixed mail and linkedIn icons on contact page, changed home links font colors

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,9 @@ import TootooLaptop from "../../content/assets/Tootoo_Asleep_Laptop.jpeg";
 import { rhythm } from "../utils/typography";
 import SEO from "../components/seo";
 import { Icon, InlineIcon } from '@iconify/react';
-// import netlifyIcon from '@iconify/icons-logos/netlify';
+import linkedinIcon from '@iconify/icons-cib/linkedin';
+import mailIcon from '@iconify/icons-feather/mail';
+import wavingHandMediumLightSkinTone from '@iconify/icons-emojione/waving-hand-medium-light-skin-tone';
 
 export default function Contact(props) {
     
@@ -17,16 +19,17 @@ export default function Contact(props) {
       <div style={{ color: `navy` }}>
       <Header headerText="Contact" />
       <SEO title="Jen Lindner | Contact" />
-      <p>I'd love to hear from you! <span>👋🏻</span></p>
-      {/* <Icon icon={netlifyIcon} /> */}
-      <p>Email: <a href="mailto:jenlindner22@gmail.com">Jenlindner22@gmail.com</a> | {` `}
-      LinkedIn: <a href="https://www.linkedin.com/in/jen-lindner13/" target="_blank" rel="noopener noreferrer">Jennifer Lindner </a> {` `} </p> 
+      <p>I'd love to hear from you! <InlineIcon icon={wavingHandMediumLightSkinTone} height="24px"/></p>
+        {/* <span>👋🏻</span></p> */}
+      <p><InlineIcon icon={mailIcon} height="24px"/> <a href="mailto:jenlindner22@gmail.com"> Jenlindner22@gmail.com</a> | {` `}
+      <InlineIcon icon={linkedinIcon} color="#0072b1" height="20px"/> <a href="https://www.linkedin.com/in/jen-lindner13/" target="_blank" rel="noopener noreferrer">Jennifer Lindner</a>
+    {` `} </p>  
       <img src={TootooLaptop} alt="Tootoo" style={{
           marginRight: rhythm(1 / 2),
           marginBottom: 20,
           minWidth: 10,
         }}/>
-      <p> I'll get back to you as soon as Tootoo gets off my laptop... </p>
+      <p> I'll get back to you as soon as Tootoo gets off my laptop! </p>
     </div>
     </Container>
     </Layout>
